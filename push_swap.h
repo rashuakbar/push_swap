b2c948088589ba411bae8ab75c1217411d7f4196
rashuakbar <victormf25@outlook.pt>
2023-10-10 13:27:29 +0100

    1st step

---
+++ push_swap.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vimendes <vimendes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 13:26:15 by vimendes          #+#    #+#             */
+/*   Updated: 2023/10/10 13:26:16 by vimendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libft/libft.h"
+
+// STACK DEFINITION
+typedef struct s_list
+{
+	int	value;
+	int	index;
+	struct s_list *next;
+};	t_list;
+
+// UTIL FUNCTIONS
+
+// SORTING ALGORITHMS
+
+// INSTRUCTION FUNCTIONS
+
+
+#endif
